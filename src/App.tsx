7d517417a10f8e02ff364a6b8b31e34a551d6b82
ramaaiofficial-beep <ramaai.official@gmail.com>
2025-10-28 19:01:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import AboutUsPage from "./pages/AboutUsPage";
+import Index from "@/pages/Index";
+import AboutUsPage from "@/pages/AboutUsPage";
 import ThrieBeamPage from "@/pages/ThrieBeamPage";
-import InquiryPage from "./pages/InquiryPage";
-import ContactUsPage from "./pages/ContactUsPage";
-import OurProducts from "./pages/OurProducts";
-import NotFound from "./pages/NotFound";
+import InquiryPage from "@/pages/InquiryPage";
+import ContactUsPage from "@/pages/ContactUsPage";
+import OurProducts from "@/pages/OurProducts";
+import NotFound from "@/pages/NotFound";
 
 const queryClient = new QueryClient();
 
